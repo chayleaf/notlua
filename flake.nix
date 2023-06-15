a15cc0b3d6d9a9fa58c8a22a18c861358ad50bd1
chayleaf <chayleaf-git@pavluk.org>
2023-06-15 08:10:01 +0700

    flake.nix: bikeshedding

--- flake.nix
+++ flake.nix
@@ -3,20 +3,19 @@
 
   outputs = { self, nixpkgs }:
     let
-      forEachSystem = func: builtins.foldl' (a: b: a // { ${b.system} = b.result; }) { } (map
-        (system: {
-          inherit system;
-          result = func { inherit system; pkgs = import nixpkgs { inherit system; }; };
-        }) [ "aarch64-linux" "aarch64-darwin" "x86_64-darwin" "x86_64-linux" ]);
+      forEachSystem = func: nixpkgs.lib.genAttrs [ "aarch64-linux" "aarch64-darwin" "x86_64-darwin" "x86_64-linux" ] (system: {
+        inherit system;
+        result = func { inherit system; pkgs = import nixpkgs { inherit system; }; };
+      });
     in
     {
-      nixosModules.default = ./default.nix;
+      nixosModules.default = ./.;
       # this isn't an officially supported third-party flake output
-      # homeManagerModules.default = ./default.nix;
-      lib = forEachSystem ({ pkgs, ... }: (import ./default.nix { inherit pkgs; inherit (pkgs) lib; }).config.notlua);
+      # homeManagerModules.default = ./.;
+      lib = forEachSystem ({ pkgs, ... }: (import ./. { inherit pkgs; inherit (pkgs) lib; }).config.notlua);
       formatter = forEachSystem ({ pkgs, ... }: pkgs.nixpkgs-fmt);
       checks.x86_64-linux.default = let pkgs = nixpkgs.legacyPackages.x86_64-linux; in pkgs.callPackage ./checks.nix {
-        flake = (pkgs.callPackage ./default.nix { }).config.notlua;
+        flake = (pkgs.callPackage ./. { }).config.notlua;
       };
     };
 }
